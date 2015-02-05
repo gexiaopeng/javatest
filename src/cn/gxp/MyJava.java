5f69b876c0d9d21258941ba7a55ed8ec3686f05d
gexiaopeng <gexiaopeng@tom.com>
2015-02-05 14:58:57 +0800

    Signed-off-by: gexiaopeng <gexiaopeng@tom.com>

---
+++ src/cn/gxp/MyJava.java
@@ -0,0 +1,25 @@
+package cn.gxp;
+/**
+ * <p>
+ * Project Name: 超级促销王
+ * <br>
+ * Description: 
+ * <br>
+ * File Name: MyJava.java
+ * <br>
+ * Copyright: Copyright (C) 2009 All Rights Reserved.
+ * <br>
+ * Company: 杭州迅博达数字传媒技术有限公司
+ * <br>
+ * @author 葛晓鹏
+ * @create time：2015-2-5 下午2:58:03
+ * @version: v2.1
+ * 
+ *       Date              Author      Version     Description
+ * ------------------------------------------------------------------
+ * 2015-2-5 下午2:58:03 |葛晓鹏    　　|v2.1        |Create
+ * 
+ */
+public class MyJava {
+
+}
