a0aecd9b7f8104520454e35bac58ad0589637aae
gexiaopeng <gexiaopeng@tom.com>
2015-03-20 16:04:25 +0800

    Signed-off-by: gexiaopeng <gexiaopeng@tom.com>

---
+++ src/cn/gxp/udp/h3client/AcDataUt.java
@@ -0,0 +1,25 @@
+package cn.gxp.udp.h3client;
+/**
+ * <p>
+ * Project Name: 超级促销王
+ * <br>
+ * Description: 
+ * <br>
+ * File Name: AcDataUt.java
+ * <br>
+ * Copyright: Copyright (C) 2009 All Rights Reserved.
+ * <br>
+ * Company: 杭州迅博达数字传媒技术有限公司
+ * <br>
+ * @author 葛晓鹏
+ * @create time：2015-3-20 下午4:04:10
+ * @version: v2.1
+ * 
+ *       Date              Author      Version     Description
+ * ------------------------------------------------------------------
+ * 2015-3-20 下午4:04:10 |葛晓鹏    　　|v2.1        |Create
+ * 
+ */
+public class AcDataUt {
+
+}
